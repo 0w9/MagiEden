9cdbe3565b1a736e2d45fba036940dfcb6150a17
Lennard Dorst <lennardsolana@gmail.com>
2022-07-20 18:40:59 +0200

    Fixing Lamports, Schema, Supabase, FP& ME API.

--- Collection.ts
+++ Collection.ts
@@ -1,28 +1,28 @@
 import fetch from "node-fetch";
+var now = require("performance-now")
 
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export async function getCollections(max_amount?: number) {
+export async function getCollections(max_amount: number = 0, limit: number = 100) {
+
     let pulled = 0;
 
-    while(true) {
-        const url = `https://api-mainnet.magiceden.dev/v2/collections?offset=${pulled}&limit=500`
+    while(pulled <= max_amount) {
+        const url = `https://api-mainnet.magiceden.dev/v2/collections?offset=${pulled}&limit=${limit}`
         let collections = await ( await fetch(url) ).json()
 
         let mapped = [];
 
         for(let collection of collections) {
-            mapped.push({
-                symbol: collection.symbol,
-                name: collection.name,
-                image: collection.image,
-            })
+            mapped.push({symbol: collection.symbol, name: collection.name, image: collection.image,})
         }
 
-        await prisma.collections.createMany({ data: mapped })
+        try {
+            await prisma.collections.createMany({ data: mapped })
+        } catch (error) {console.log(`Error ${error} occurred while creating collections.`)}
 
-        pulled = pulled + mapped.length;
+        pulled += collections.length;
     }
 }
 
@@ -33,19 +33,11 @@ export async function SaveCollectionFP() {
         for (let i = 0; i < 10; i++) {
             let url = `https://api-mainnet.magiceden.dev/v2/collections/${collection.symbol}/stats`
             let response = (await (await fetch(url)).json())
-            let FP = response.floorPrice;
-            console.log(FP);
+            let FP = response.floorPrice / 1000000000;
 
-            // @ts-ignore
             console.log(collection)
-            await prisma.floor_prices.create({
-                data: {
-                    id: 1,
-                    symbol: collection.symbol,
-                    timestamp_ms: 1,
-                    fp_lamports: FP,
-                }
-            })
+            // @ts-ignore
+            await prisma.floor_prices.create({data: {symbol: collection.symbol, timestamp_ms: new Date().getTime(), fp_lamports: FP,}})
 
             await new Promise(r => setTimeout(r, 5000));
         }
@@ -53,9 +45,8 @@ export async function SaveCollectionFP() {
     }
 }
 
-export async function hacking() {
-    const res = await fetch("https://api-mainnet.magiceden.io/rpc/getCollectionMetricsSeries/danger_valley_ducks?edge_cache=trueres.json()")
-}
-//getCollections();
-//getCollections(500);
+// getCollections will round to the next 100. So the limit can be set to 100 to prevent a) API spams and b) long loading times.
+getCollections(200);
+
+console.log(`DONE! Starting to save FPs.`)
 SaveCollectionFP()
