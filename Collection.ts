5d30dc8e2534985ab6848dd911083eddbc6226cc
Lennard Dorst <lennardsolana@gmail.com>
2022-07-20 18:15:18 +0200

    Chaning the Supabase Url.

--- Collection.ts
+++ Collection.ts
@@ -26,27 +26,36 @@ export async function getCollections(max_amount?: number) {
     }
 }
 
-export async function SaveCollectionFP(symbol: string) {
-    while (true) {
-        let url = `https://api-mainnet.magiceden.dev/v2/collections/${symbol}/stats`
-        let response =  (await (await fetch(url)).json())
-        let FP = response.floorPrice;
-        console.log(FP);
+export async function SaveCollectionFP() {
+    const collections = await prisma.collections.findMany();
 
-        // @ts-ignore
-        await prisma.floor_prices.create({
-            data: {
-                symbol,
-                timestamp_ms: Date.now(),
-                fp_lamports: FP,
-            }
-        })
+    for(let collection of collections) {
+        for (let i = 0; i < 10; i++) {
+            let url = `https://api-mainnet.magiceden.dev/v2/collections/${collection.symbol}/stats`
+            let response = (await (await fetch(url)).json())
+            let FP = response.floorPrice;
+            console.log(FP);
 
-        await new Promise(r => setTimeout(r, 5000));
+            // @ts-ignore
+            console.log(collection)
+            await prisma.floor_prices.create({
+                data: {
+                    id: 1,
+                    symbol: collection.symbol,
+                    timestamp_ms: 1,
+                    fp_lamports: FP,
+                }
+            })
+
+            await new Promise(r => setTimeout(r, 5000));
+        }
+
     }
 }
 
+export async function hacking() {
+    const res = await fetch("https://api-mainnet.magiceden.io/rpc/getCollectionMetricsSeries/danger_valley_ducks?edge_cache=trueres.json()")
+}
 //getCollections();
 //getCollections(500);
-
-SaveCollectionFP("maskedmvmnt")
+SaveCollectionFP()
